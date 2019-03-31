c0087e52cf083aefac81e1a3194458d735cbad2f
Justine B <justinegayton@gmail.com>
2019-03-31 20:05:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ function driversWithRevenueOver(drivers, revenue) {
   return drivers;
 }
 
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+ driverName = driversWithRevenueOver(drivers, revenue)
+  return driverName.map(driver => driver.name)
+}
+
 function exactMatch(drivers, object){
   return drivers.filter(function(driver){
     match = false
