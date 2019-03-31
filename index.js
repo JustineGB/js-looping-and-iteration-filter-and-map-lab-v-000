bd10f39dd5cce023d82f05452f2d968876d44fe6
Justine B <justinegayton@gmail.com>
2019-03-31 19:41:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const drivers = []
 
 function driversWithRevenueOver(drivers, revenue) {
-  drivers = drivers.filter(drivers => driver.revenue > revenue);
+  drivers = drivers.filter(driver => drive.revenue > revenue);
   return drivers;
 }
 
