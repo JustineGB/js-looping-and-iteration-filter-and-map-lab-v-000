d3696374041bb1c0a87ce97bf6c364e02bed6b9f
Justine B <justinegayton@gmail.com>
2019-03-31 19:54:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function driverNamesWithRevenueOver(drivers, revenue) {
 
 
 function exactMatch (drivers) {
-  drivers = drivers.where(driver === driver)
+  drivers = drivers.match()
   return drivers;
 }
